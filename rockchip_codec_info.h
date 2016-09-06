1da6112da6e44582f65a0a2b283ceb4787e17875
Randy Li <randy.li@rock-chips.com>
2016-09-06 17:10:05 +0800

    va: codec info encoder
    Signed-off-by: Randy Li <randy.li@rock-chips.com>

--- rockchip_codec_info.h
+++ rockchip_codec_info.h
@@ -71,10 +71,79 @@ struct decode_state {
 	struct buffer_store *image_data;
 };
 
+struct encode_state {
+	struct codec_state_base base;
+	struct buffer_store *seq_param;
+	struct buffer_store *pic_param;
+	struct buffer_store *pic_control;
+	struct buffer_store *iq_matrix;
+	struct buffer_store *q_matrix;
+	struct buffer_store *huffman_table;
+
+	struct buffer_store **slice_params;
+	int max_slice_params;
+	int num_slice_params;
+
+	/* for ext */
+	struct buffer_store *seq_param_ext;
+	struct buffer_store *pic_param_ext;
+	struct buffer_store *packed_header_param[5];
+	struct buffer_store *packed_header_data[5];
+
+	struct buffer_store **slice_params_ext;
+	int max_slice_params_ext;
+	int num_slice_params_ext;
+
+	struct buffer_store *encmb_map;
+
+	/* Check the user-configurable packed_header attribute.
+	 * Currently it is mainly used to check whether the packed slice_header data
+	 * is provided by user or the driver.
+	 * TBD: It will check for the packed SPS/PPS/MISC/RAWDATA and so on.
+	 */
+	unsigned int packed_header_flag;
+	/* For the packed data that needs to be inserted into video clip */
+	/* currently it is mainly to track packed raw data and packed slice_header data. */
+	struct buffer_store **packed_header_params_ext;
+	int max_packed_header_params_ext;
+	int num_packed_header_params_ext;
+	struct buffer_store **packed_header_data_ext;
+	int max_packed_header_data_ext;
+	int num_packed_header_data_ext;
+
+	/* the index of current vps and sps ,special for HEVC*/
+	int vps_sps_seq_index;
+	/* the index of current slice */
+	int slice_index;
+	/* the array is determined by max_slice_params_ext */
+	int max_slice_num;
+	/* This is to store the first index of packed data for one slice */
+	int *slice_rawdata_index;
+	/* This is to store the number of packed data for one slice.
+	 * Both packed rawdata and slice_header data are tracked by this
+	 * this variable. That is to say: When one packed slice_header is parsed,
+	 * this variable will also be increased.
+	 */
+	int *slice_rawdata_count;
+
+	/* This is to store the index of packed slice header for one slice */
+	int *slice_header_index;
+
+	int last_packed_header_type;
+
+	struct buffer_store *misc_param[16];
+
+	VASurfaceID current_render_target;
+	struct object_surface *input_yuv_object;
+	struct object_surface *reconstructed_object;
+	struct object_buffer *coded_buf_object;
+	struct object_surface *reference_objects[16]; /* Up to 2 reference surfaces are valid for MPEG-2,*/
+};
+
 union codec_state {
 	struct codec_state_base base;
 	struct decode_state decode;
-	struct codec_state_base encode;
+	struct encode_state encode;
 	struct codec_state_base proc;
 };
 
