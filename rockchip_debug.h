7191db8a93f9f5a2dfe000eb3d152d36baefe08f
ayaka <ayaka@soulik.info>
2016-09-12 07:10:09 +0800

    debug: add warn_once()
    Signed-off-by: ayaka <ayaka@soulik.info>

--- rockchip_debug.h
+++ rockchip_debug.h
@@ -2,6 +2,14 @@
 #define _ROCKCHIP_DEBUG_H_
 #include "common.h"
 
+#define WARN_ONCE(...) do {				\
+		static bool g_once = true;				\
+		if (g_once) {						\
+			g_once = false;					\
+			fprintf(stderr, "WARNING: " __VA_ARGS__);	\
+		}							\
+	} while(0)
+
 void rk_error_msg(const char *msg, ...);
 void rk_info_msg(const char *msg, ...);
 
